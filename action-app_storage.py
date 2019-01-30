6234e01b36efd3b515c7dae107d107b293fba71c
Anja Freudenreich <freudenreich.anja@gmail.com>
2019-01-30 23:36:00 +0100

    small test was working fine. Now trying with storage class

--- action-app_storage.py
+++ action-app_storage.py
@@ -42,8 +42,8 @@ class Storageassistant(object):
         hermes.publish_end_session(intent_message.session_id, "")
 
         # action code goes here...
-        # answer = self.mystorage.testing_dummy_function(intent_message.intent) #addEntryToVorraete(intent_message.intent)
-        answer = "hinzufügen"
+        answer = self.mystorage.testing_dummy_function(intent_message.intent) #addEntryToVorraete(intent_message.intent)
+        # answer = "hinzufügen"
         # if need to speak the execution result by tts
         hermes.publish_start_session_notification(intent_message.site_id, answer, "Storage_APP")
 
@@ -63,8 +63,8 @@ class Storageassistant(object):
 
         # action code goes here...
         # answer = self.mystorage.getAmountOf(intent_message)
-        # answer = self.mystorage.testing_dummy_function(intent_message.intent)
-        answer = "Wieviel"
+        answer = self.mystorage.testing_dummy_function(intent_message.intent)
+        # answer = "Wieviel"
 
         # if need to speak the execution result by tts
         hermes.publish_start_session_notification(intent_message.site_id, answer, "Storage_APP")
